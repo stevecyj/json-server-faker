1c3db3b4add1772082b11d7c98a455a633e3c282
Steve <ycsteven.tsao@gmail.com>
2020-03-04 15:10:48 +0800

    faker.js修改

--- db.js
+++ db.js
@@ -1,19 +1,39 @@
 //db.js
 const faker = require('faker');
+const efaker = require('faker/locale/en');
+faker.locale = 'zh_TW';
 
 function generateData() {
     const data = [];
 
-    for (let id = 0; id < 10; id++) {
-        let firstName = faker.name.firstName();
-        let lastName = faker.name.firstName();
+    for (let id = 1; id < 50; id++) {
+        let name = faker.name.firstName() + faker.name.lastName();
+        let ename = efaker.name.lastName();
         let phoneNumber = faker.phone.phoneNumberFormat();
+        let email = faker.internet.email();
+        let sex = faker.random.word();
+        let city = faker.address.city();
+        let township = faker.address.cityPrefix() + faker.address.citySuffix();
+        let postcode = faker.address.zipCode();
+        let address = faker.address.streetAddress();
+        let notes = faker.random.words();
+        let created_at = faker.date.past();
+        let updated_at = faker.date.future();
 
         data.push({
             id: id,
-            first_name: firstName,
-            last_name: lastName,
-            phone: phoneNumber
+            name: name,
+            ename: ename,
+            phone: phoneNumber,
+            email: email,
+            sex: sex,
+            city: city,
+            township: township,
+            postcode: postcode,
+            address: address,
+            notes: notes,
+            created_at: created_at,
+            updated_at: updated_at
         });
     }
     return { customers: data };
